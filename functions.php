0abdfcbda1948f897a0becdcbe9508ad012444b9
Greg S <greg@evias.be>
2019-01-02 01:06:34 +0100

    use plugin for notifications

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-
+/*
 // This would normally be enqueued as a file, but for the sake of ease we will just print to the footer
 function add_orders_ajax_call(){ ?>
  
@@ -83,4 +83,5 @@ function check_orders() {
  
 // This bit is a special action hook that works with the WordPress AJAX functionality. 
 add_action( 'wp_ajax_check_orders', 'check_orders' );
+*/
 ?>
