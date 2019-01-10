4ae81c32f99f9e33c24c3efbafd09426a74c5e91
Greg S <greg@evias.be>
2019-01-10 17:58:32 +0100

    fix delivery callback handler

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ require_once dirname( __FILE__ ) . '/includes/class-evs-css-injector.php';
 
 // Inject CSS and JS into Backend Theme
 add_action('wp_enqueue_scripts', 'EVS_CSS_Injector::setStylesheet', PHP_INT_MAX);
-add_action('in_admin_footer', 'EVS_JS_Injector::injectOrderListener');
+add_action('in_admin_footer', 'EVS_JS_Injector::getOrderListener');
 
 // Register admin-ajax.php?action=XX calls
 add_action('wp_ajax_check_orders', 'EVS_Order_Listener::getCountOrders' );
@@ -40,7 +40,7 @@ add_action('wp_ajax_is_delivering', 'EVS_Status_Listener::isDelivering' );
 add_action('rest_api_init', function () {
     register_rest_route('restaurant/v1', '/delivery', array(
         'methods' => 'GET',
-        'callback' => 'is_delivering',
+        'callback' => 'EVS_Status_Listener::isDelivering',
     ));
 });
 ?>
