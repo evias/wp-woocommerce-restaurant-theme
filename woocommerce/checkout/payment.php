ade2088030d6e16710ffc6225fbc5386976bf9b6
Greg S <greg@evias.be>
2019-07-23 13:46:14 +0200

    fix integer cast

--- woocommerce/checkout/payment.php
+++ woocommerce/checkout/payment.php
@@ -81,7 +81,7 @@ jQuery(document).ready(function($) {
 		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
 
 		<?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
-        <?php echo '<div id="delivery-status-msg" class="hide alert-danger">&nbsp;</div>'; ?>
+        <?php echo '<div id="delivery-status-msg" class="hide alert-danger" style="font-weight: 800; color: #f00;">&nbsp;</div>'; ?>
 
 		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
 
